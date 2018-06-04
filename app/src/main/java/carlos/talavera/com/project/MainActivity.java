a686d4582880cece13539172b0c832129262c4f7
tala79 <tala@ono.com>
2018-06-04 18:24:31 +0200

    Añadido "compartir con mi app"

--- app/src/main/java/carlos/talavera/com/project/MainActivity.java
+++ app/src/main/java/carlos/talavera/com/project/MainActivity.java
@@ -1,13 +1,25 @@
 package carlos.talavera.com.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class
+MainActivity extends AppCompatActivity {
+
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        onNewIntent(getIntent());
+    }
+
+    @Override
+    public void onNewIntent(Intent intent){
+        type = intent.getType();
+        System.out.println("Tipo del intent" + type);
+
     }
 }
